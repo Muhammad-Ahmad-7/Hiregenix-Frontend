e827e4084132dcdf152fda3167794a8cd02ec138
Abdullah Usman <ib6457345@gmail.com>
2025-12-10 10:17:42 -0500

    Redirection...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,20 @@
-// export default function App() {
-//   const isClient = false;
-//   return (
-//     <div className="h-screen w-full flex justify-center items-center ">
-//       HIREGENIX
-//     </div>
-//   );
-// }
 "use client";
-import Left1 from "@/component/pages/login/Left1";
-// import Left3 from "@/component/pages/login/Left3";
-import SignInWrapper from "@/component/pages/login/SignInWrapper";
-// import { Button, DatePicker } from "antd";
-import React from "react";
 
-import sign_in from "../../public/main.png";
-import LoginScreen from "@/component/pages/login/Login";
-const Home = () => (
-  <SignInWrapper img={sign_in}>
-    {/* <Left1 /> */}
-    <LoginScreen />
-    {/* <Left3 /> */}
-  </SignInWrapper>
-);
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("https://hiregenx.vercel.app/");
+  }, [router]);
+
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-xl font-semibold">Redirecting...</h1>
+    </div>
+  );
+};
 
 export default Home;
